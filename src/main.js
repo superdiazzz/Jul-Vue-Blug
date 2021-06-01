35c94aa6e8ab38054e27fc79010f642a7a88b48b
nanda <zulhija.nanda@gmail.com>
2021-06-01 09:20:54 +0700

    init commit

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueDisqus from 'vue-disqus'
 
-createApp(App).use(router).mount('#app')
+createApp(App)
+    .use(VueDisqus, { shortname: 'skillbaru-com' })
+    .use(router).mount('#app')
